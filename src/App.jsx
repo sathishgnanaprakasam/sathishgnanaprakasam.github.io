1dee2555265fb993b8b4bc8f47aab1d5295120da
githubscientist <sathish.datascientist@gmail.com>
2024-10-25 16:59:55 +0530

    react app test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <center><h1>Sathish Gnanaprasam's Portfolio</h1></center>
+      <center><h4>Full Stack Developer</h4></center>
+    </div>
+  )
+}
+
+export default App
